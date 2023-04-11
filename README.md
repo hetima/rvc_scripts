ee56578dbd2006618d16608512fa36362aadd980
hetima <33700+hetima@users.noreply.github.com>
2023-04-11 12:53:38 +0900

    0.1.3 値の保存中止

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ RVCにちょっと便利な機能を追加するTampermonkeyスクリプトで�
 Tampermonkey機能拡張を使用している状態で [rvc_tweaks.user.js](https://github.com/hetima/rvc_jp_translate/raw/main/rvc_tweaks.user.js) を開くとインストールできます。
 
 - ""で囲まれたファイルパスをペーストすると自動的に"を除去します
-- save_every_epoch、total_epoch、batch_sizeの値を保存して次回開いたときに復元します（スライダーでの変更は記録されません。保存したい値を直接数値入力してください）
+- ~~save_every_epoch、total_epoch、batch_sizeの値を保存して次回開いたときに復元します（スライダーでの変更は記録されません。保存したい値を直接数値入力してください）~~
 
--- rvc_tweaks.user.js
+++ rvc_tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         rvc_tweaks
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.1.3
 // @description  rvc_tweaks
 // @author       hetima
 // @match        http://127.0.0.1:7865/
@@ -11,7 +11,7 @@
 // @grant GM_getValue
 // ==/UserScript==
 
-
+// 0.1.3 save_every_epoch、total_epoch、batch_sizeの保存対応停止
 // 0.1.2 save_every_epoch、total_epoch、batch_sizeの保存値をスライダーにも反映
 
 (function () {
@@ -57,55 +57,55 @@
             });
 
             // トレーニング設定保存
-            elementList = document.querySelector('gradio-app').shadowRoot.querySelectorAll("span");
-            elementList.forEach(function (itm) {
-                var numInput = getNumInputForSpan(itm, 'save_every_epoch');
-                var rangeInput;
-                if (numInput != null){
-                    var save_every_epoch=GM_getValue("save_every_epoch");
-                    if (!Number.isNaN(parseInt(save_every_epoch))){
-                        numInput.value = save_every_epoch;
-                    }
-                    numInput.addEventListener('change', function (evt) {
-                        GM_setValue("save_every_epoch", evt.target.value);
-                    });
-                    rangeInput = getRangeInputForNumInput(numInput);
-                    if (rangeInput != null) {
-                        rangeInput.value = save_every_epoch;
-                    }
-                }
+            // elementList = document.querySelector('gradio-app').shadowRoot.querySelectorAll("span");
+            // elementList.forEach(function (itm) {
+            //     var numInput = getNumInputForSpan(itm, 'save_every_epoch');
+            //     var rangeInput;
+            //     if (numInput != null){
+            //         var save_every_epoch=GM_getValue("save_every_epoch");
+            //         if (!Number.isNaN(parseInt(save_every_epoch))){
+            //             numInput.value = save_every_epoch;
+            //         }
+            //         numInput.addEventListener('change', function (evt) {
+            //             GM_setValue("save_every_epoch", evt.target.value);
+            //         });
+            //         rangeInput = getRangeInputForNumInput(numInput);
+            //         if (rangeInput != null) {
+            //             rangeInput.value = save_every_epoch;
+            //         }
+            //     }
 
-                numInput = getNumInputForSpan(itm, 'total_epoch');
-                if (numInput != null) {
-                    var total_epoch = GM_getValue("total_epoch");
-                    if (!Number.isNaN(parseInt(total_epoch))) {
-                        numInput.value = total_epoch;
-                    }
-                    numInput.addEventListener('change', function (evt) {
-                        GM_setValue("total_epoch", evt.target.value);
-                    });
-                    rangeInput = getRangeInputForNumInput(numInput);
-                    if (rangeInput != null) {
-                        rangeInput.value = total_epoch;
-                    }
-                }
+            //     numInput = getNumInputForSpan(itm, 'total_epoch');
+            //     if (numInput != null) {
+            //         var total_epoch = GM_getValue("total_epoch");
+            //         if (!Number.isNaN(parseInt(total_epoch))) {
+            //             numInput.value = total_epoch;
+            //         }
+            //         numInput.addEventListener('change', function (evt) {
+            //             GM_setValue("total_epoch", evt.target.value);
+            //         });
+            //         rangeInput = getRangeInputForNumInput(numInput);
+            //         if (rangeInput != null) {
+            //             rangeInput.value = total_epoch;
+            //         }
+            //     }
 
-                numInput = getNumInputForSpan(itm, 'batch_size');
-                if (numInput != null) {
-                    var batch_size = GM_getValue("batch_size");
-                    if (!Number.isNaN(parseInt(batch_size))) {
-                        numInput.value = batch_size;
-                    }
-                    numInput.addEventListener('change', function (evt) {
-                        GM_setValue("batch_size", evt.target.value);
-                    });
-                    rangeInput = getRangeInputForNumInput(numInput);
-                    if (rangeInput != null) {
-                        rangeInput.value = batch_size;
-                    }
-                }
+            //     numInput = getNumInputForSpan(itm, 'batch_size');
+            //     if (numInput != null) {
+            //         var batch_size = GM_getValue("batch_size");
+            //         if (!Number.isNaN(parseInt(batch_size))) {
+            //             numInput.value = batch_size;
+            //         }
+            //         numInput.addEventListener('change', function (evt) {
+            //             GM_setValue("batch_size", evt.target.value);
+            //         });
+            //         rangeInput = getRangeInputForNumInput(numInput);
+            //         if (rangeInput != null) {
+            //             rangeInput.value = batch_size;
+            //         }
+            //     }
 
-            });
+            // });
         }
 
     }
