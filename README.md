463fb8cabe10d646c43a23f37a58861b84bbc422
hetima <33700+hetima@users.noreply.github.com>
2023-04-12 17:18:00 +0900

    特徴量ファイルパス自動設定

--- README.md
+++ README.md
@@ -14,5 +14,11 @@ RVCにちょっと便利な機能を追加するTampermonkeyスクリプトで�
 Tampermonkey機能拡張を使用している状態で [rvc_tweaks.user.js](https://github.com/hetima/rvc_jp_translate/raw/main/rvc_tweaks.user.js) を開くとインストールできます。
 
 - ""で囲まれたファイルパスをペーストすると自動的に"を除去します
-- ~~save_every_epoch、total_epoch、batch_sizeの値を保存して次回開いたときに復元します（スライダーでの変更は記録されません。保存したい値を直接数値入力してください）~~
+- 特徴量検索用ファイルパス（added_XXXX.index）と特徴量ファイルパス（total_fea.npy）を自動設定
+
+モデルを変更すると、対応する特徴量ファイルパスを自動で設定します。  
+推論実行タブの最下段にフォルダパスを設定する項目が追加されます。そこで設定したフォルダの中に「モデル名.index」「モデル名.npy」とリネームして配置してください。  
+例えばフォルダパスを `D:\RVC-beta\weights\subdata` と指定したら `added_XXXX.index` と `total_fea.npy` を `モデル名.index` と `モデル名.npy`にリネームして `D:\RVC-beta\weights\subdata` に入れてください。  
+ただし、モデル名が数字で終わる場合は数字を除いた名前にしてください(xxx100→xxx つまり「xxx」と「xxx数字」ではファイルを共用します)。  
+ファイルが存在するかどうかはチェックせず強制的に設定変更するのでご注意ください。
 
--- rvc_tweaks.user.js
+++ rvc_tweaks.user.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name         rvc_tweaks
 // @namespace    http://tampermonkey.net/
-// @version      0.1.3
+// @version      0.2.0
 // @description  rvc_tweaks
 // @author       hetima
 // @match        http://127.0.0.1:7865/
 // @match        https://*.gradio.live/
 // @run-at       document-end
-// @grant GM_setValue
-// @grant GM_getValue
 // ==/UserScript==
 
+// 0.2.0 特徴量ファイルパスの自動設定を追加
 // 0.1.3 save_every_epoch、total_epoch、batch_sizeの保存対応停止
 // 0.1.2 save_every_epoch、total_epoch、batch_sizeの保存値をスライダーにも反映
 
@@ -43,6 +42,7 @@
         if (elementList.length > 0) {
             console.log("rvc_tweaks inited");
             inited = true;
+
             // テキストが""で囲まれてたら"を削除する
             elementList.forEach(function (itm) {
                 //itm.onkeyup = handleChangeText;
@@ -56,59 +56,145 @@
                 });
             });
 
-            // トレーニング設定保存
-            // elementList = document.querySelector('gradio-app').shadowRoot.querySelectorAll("span");
-            // elementList.forEach(function (itm) {
-            //     var numInput = getNumInputForSpan(itm, 'save_every_epoch');
-            //     var rangeInput;
-            //     if (numInput != null){
-            //         var save_every_epoch=GM_getValue("save_every_epoch");
-            //         if (!Number.isNaN(parseInt(save_every_epoch))){
-            //             numInput.value = save_every_epoch;
-            //         }
-            //         numInput.addEventListener('change', function (evt) {
-            //             GM_setValue("save_every_epoch", evt.target.value);
-            //         });
-            //         rangeInput = getRangeInputForNumInput(numInput);
-            //         if (rangeInput != null) {
-            //             rangeInput.value = save_every_epoch;
-            //         }
-            //     }
 
-            //     numInput = getNumInputForSpan(itm, 'total_epoch');
-            //     if (numInput != null) {
-            //         var total_epoch = GM_getValue("total_epoch");
-            //         if (!Number.isNaN(parseInt(total_epoch))) {
-            //             numInput.value = total_epoch;
-            //         }
-            //         numInput.addEventListener('change', function (evt) {
-            //             GM_setValue("total_epoch", evt.target.value);
-            //         });
-            //         rangeInput = getRangeInputForNumInput(numInput);
-            //         if (rangeInput != null) {
-            //             rangeInput.value = total_epoch;
-            //         }
-            //     }
-
-            //     numInput = getNumInputForSpan(itm, 'batch_size');
-            //     if (numInput != null) {
-            //         var batch_size = GM_getValue("batch_size");
-            //         if (!Number.isNaN(parseInt(batch_size))) {
-            //             numInput.value = batch_size;
-            //         }
-            //         numInput.addEventListener('change', function (evt) {
-            //             GM_setValue("batch_size", evt.target.value);
-            //         });
-            //         rangeInput = getRangeInputForNumInput(numInput);
-            //         if (rangeInput != null) {
-            //             rangeInput.value = batch_size;
-            //         }
-            //     }
-
-            // });
+            setupTPath();
+            //動くようになったけどローカライズの種類に依存するので
+            // setupTrainCount();
         }
 
     }
+
+    function setupTrainCount() {
+
+        //トレーニング設定保存
+        const elementList = document.querySelector('gradio-app').shadowRoot.querySelectorAll("span");
+        elementList.forEach(function (itm) {
+            var numInput = getNumInputForSpan(itm, 'save_every_epoch');
+            var rangeInput;
+            if (numInput != null){
+                var save_every_epoch = localStorage.getItem("rvc_tweaks_save_every_epoch");
+                if (!Number.isNaN(parseInt(save_every_epoch))){
+                    numInput.value = save_every_epoch;
+                    numInput.dispatchEvent(new Event("input"))
+                }
+                numInput.addEventListener('change', function (evt) {
+                    localStorage.setItem("rvc_tweaks_save_every_epoch", evt.target.value);
+                });
+                rangeInput = getRangeInputForNumInput(numInput);
+                // if (rangeInput != null) {
+                //     rangeInput.value = save_every_epoch;
+                // }
+            }
+
+            numInput = getNumInputForSpan(itm, 'total_epoch');
+            if (numInput != null) {
+                var total_epoch = localStorage.getItem("rvc_tweaks_total_epoch");
+                if (!Number.isNaN(parseInt(total_epoch))) {
+                    numInput.value = total_epoch;
+                    numInput.dispatchEvent(new Event("input"))
+                }
+                numInput.addEventListener('change', function (evt) {
+                    localStorage.setItem("rvc_tweaks_total_epoch", evt.target.value);
+                });
+                // rangeInput = getRangeInputForNumInput(numInput);
+                // if (rangeInput != null) {
+                //     rangeInput.value = total_epoch;
+                // }
+            }
+
+            numInput = getNumInputForSpan(itm, 'batch_size');
+            if (numInput != null) {
+                var batch_size = localStorage.getItem("rvc_tweaks_batch_size");
+                if (!Number.isNaN(parseInt(batch_size))) {
+                    numInput.value = batch_size;
+                    numInput.dispatchEvent(new Event("input"))
+                }
+                numInput.addEventListener('change', function (evt) {
+                    localStorage.setItem("rvc_tweaks_batch_size", evt.target.value);
+                });
+                // rangeInput = getRangeInputForNumInput(numInput);
+                // if (rangeInput != null) {
+                //     rangeInput.value = batch_size;
+                // }
+            }
+
+        });
+    }
+
+    //特徴量ファイルパス
+    const addedIndexTextareas = new Array();
+    const totalFeaTextareas = new Array();
+    function setupTPath(){
+        
+        //textareaを探す
+        var elementList = document.querySelector('gradio-app').shadowRoot.querySelectorAll("textarea");
+        var textareaCount = 0;
+        if (elementList.length > 0) {
+            elementList.forEach(function (itm) {
+                if (itm.value.indexOf(".index") != -1) {
+                    addedIndexTextareas.push(itm);
+                    textareaCount++;
+                } else if (itm.value.indexOf("total_fea.npy") != -1) {
+                    totalFeaTextareas.push(itm);
+                    textareaCount++;
+                }
+            });
+        }
+        if (textareaCount < 4){
+            return;
+        }
+
+        //設定UIを追加
+        const view = addViewToTab(0, "rvc_tweaks");
+        const tPathTextarea = addTextareaToView(view, "特徴量検索用ファイルパス（added_XXXX.index）と特徴量ファイルパス（total_fea.npy）を自動設定するためのフォルダパス（例：D:\\RVC-beta\\weights\\subdata）<br>このフォルダの中に「モデル名.index」「モデル名.npy」とリネームして配置してください。ただし、モデル名が数字で終わる場合は数字を除いた名前にしてください(xxx100→xxx)<br>ファイルが存在するかどうかはチェックせず強制的に設定変更するのでご注意ください。");
+        tPathTextarea.value = localStorage.getItem("rvc_tweaks_t_path");
+        tPathTextarea.addEventListener('change', function (evt) {
+            localStorage.setItem("rvc_tweaks_t_path", evt.target.value);
+        });
+        tPathTextarea.addEventListener('keyup', function (evt) {
+            var tgt = evt.target;
+            var txt = tgt.value;
+            var char = txt.slice(0, 1);
+            if (char == '"' && char == txt.slice(-1)) {
+                tgt.value = txt.slice(1, -1);
+                localStorage.setItem("rvc_tweaks_t_path", evt.target.value);
+            }
+        });
+
+        //モデル選択したらパスを更新
+        const slct = document.querySelector('gradio-app').shadowRoot.querySelector(".tabitem>div>div>div>div>label>select");
+        if(slct){
+            slct.addEventListener('change', function (evt) {
+
+                let tPath = localStorage.getItem("rvc_tweaks_t_path");
+                if(tPath.length <= 0){
+                    return;
+                }
+                if (tPath.slice(-1) == "\\"){
+                    tPath = tPath.slice(0, -1);
+                }
+                if (evt.target.value.lastIndexOf(".")>0){
+                    //拡張子と末尾の数字を除去
+                    let result = evt.target.value.substring(0, evt.target.value.lastIndexOf(".")).replace(/\d+$/, "");
+                    let addIndexPath = tPath + "\\" + result + ".index";
+                    let totalFeaPath = tPath + "\\" + result + ".npy";
+                    addedIndexTextareas.forEach((element) => { 
+                        element.value = addIndexPath;
+                        //Gradioに変更したことを認識させる
+                        element.dispatchEvent(new Event("input"));
+                    });
+                    totalFeaTextareas.forEach((element) => {
+                        element.value = totalFeaPath;
+                        //Gradioに変更したことを認識させる
+                        element.dispatchEvent(new Event("input"));
+                    });
+                }
+            });
+        }
+
+    }
+
+
 
     const observer = new MutationObserver(records => {
         setup();
@@ -117,11 +203,52 @@
         childList: true,
         subtree: true
     };
-    observer.observe(document.querySelector('gradio-app').shadowRoot, options);
-    setTimeout(function () {
-        //時間がたったら監視停止
-        observer.disconnect();
-    }, 8000);
+
+    const shadowRoot = document.querySelector('gradio-app').shadowRoot
+    if (shadowRoot == undefined){
+        console.log("shadowRoot is undefined");
+        setTimeout(function () {
+            setup();
+        }, 1500);
+    }else{
+        observer.observe(document.querySelector('gradio-app').shadowRoot, options);
+        setTimeout(function () {
+            //時間がたったら監視停止
+            observer.disconnect();
+        }, 8000);
+    }
+
+    function addViewToTab(index, title){
+        const elementList = document.querySelector('gradio-app').shadowRoot.querySelectorAll(".tabitem");
+        if (elementList.length > index) {
+            const tab = elementList[index].childNodes[0];
+            const view = document.createElement('div');
+            view.classList.add("svelte-10ogue4");
+            const label = document.createElement('div');
+            label.innerHTML = "<p>" + title + "</p>";
+            view.insertBefore(label, null);
+            tab.insertBefore(view, null);
+            return view;
+        }
+    }
+    function addTextareaToView(view, title){
+        const pane = document.createElement('div');
+        //pane.classList.add("flex","row", "w-full", "flex-wrap", "gap-4");
+        //pane.classList.add("gr-form", "overflow-hidden", "flex", "border-solid", "border", "bg-gray-200", "dark:bg-gray-700", "gap-px", "rounded-lg", "flex-wrap");
+        const textarea = document.createElement('textarea');
+        textarea.classList.add("scroll-hide", "block", "gr-box", "gr-input", "w-full", "gr-text-input")
+        //textarea.style = "overflow-y: scroll; height: 42px;";
+
+        const label = document.createElement('div');
+        label.innerHTML = "<span class=\"text-gray-500 text-[0.855rem]\">" + title + "</span>";
+        label.classList.add("block", "w-full");
+        
+        label.insertBefore(textarea, null);
+        pane.insertBefore(label, null);
+        view.insertBefore(pane, null);
+        return textarea;
+    }
+
 
 })();
 
